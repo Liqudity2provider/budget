b6c511b0e43bb3137000028704e5941dcab2eb48
Oleksandr Hrechenko <alex.grechenko@gmail.com>
2022-07-16 11:47:34 +0200

    Fix problem with path (logout)

--- budget_django/middleware/jwt_middleware.py
+++ budget_django/middleware/jwt_middleware.py
@@ -10,18 +10,20 @@ class JWTMiddleware:
         # One-time configuration and initialization.
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
+        """
+        If path not null -> check if path in paths that not require login
+        """
         self._token = None
         formatted_path = str(request.path).replace("/", "")
-        skip_to_logout = [i for i in SKIP_TOKEN_PATHS if i in formatted_path]
-        if skip_to_logout:
-            _token = refresh_token_or_redirect(request)
-            if not isinstance(_token, str):
-                return redirect('logout')
-            user = user_from_token(_token)
-            request.jwt_user = user
-            request._token = _token
+        if formatted_path:
+            redirect_to_logout = [i for i in SKIP_TOKEN_PATHS if i in formatted_path]
+            if not redirect_to_logout or not formatted_path:
+                _token = refresh_token_or_redirect(request)
+                if not isinstance(_token, str):
+                    return redirect('logout')
+                user = user_from_token(_token)
+                request.jwt_user = user
+                request._token = _token
 
         response = self.get_response(request)
 
